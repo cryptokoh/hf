7dcd17836fd069806467bc4a89af5ac8eb5a8a74
cryptokoh <cryptokoh@gmail.com>
2024-11-04 23:28:23 -0600

    Up

--- src/index.jsx
+++ src/index.jsx
@@ -3,18 +3,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css'; // Import Tailwind CSS
+import './index.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
 
-// Safely access the Ethereum object
-if (typeof window.ethereum !== 'undefined') {
-  console.log('Ethereum object detected:', window.ethereum);
-} else {
-  console.log('No Ethereum provider found. Install MetaMask.');
-}
+// Configure chains and create a client
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.goerli],
+  [publicProvider()]
+);
 
+const { connectors } = getDefaultWallets({
+  appName: 'Hand Protocol',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+// Render the React application with RainbowKit and Wagmi
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>,
   document.getElementById('root')
 );
